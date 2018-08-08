28c02f390918942a85c192d49e18583d5c88bbf7
NicksComingUp <niclaus.a@gmail.com>
2018-08-08 09:05:06 -0500

    updated navigation in starter page and added adventure component

--- effects.js
+++ effects.js
@@ -1,7 +1,22 @@
-//lert('Hello World im home');
+//alert('Hello World im home');
+
 
 $('.nav__menu-link').on('click', function(){
   //console.log("working");
   // $('.nav__list').fadeToggle();
   $('.nav__list').fadeToggle(1000);
 });
+
+const sizeUpdate = ()=>{
+  let width = document.documentElement.clientWidth;
+  let list =  document.querySelector('.nav__list');
+
+  if(width > 600) return list.style.display = 'flex';
+
+  return  list.style.display = 'none';
+
+}
+/*
+  triggers an event if the screen size change
+*/
+window.addEventListener('resize',sizeUpdate);
